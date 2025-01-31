07279551340702fe8e9eaadb64237663b582da61
Haruki1090 <haruharu.com.109607@gmail.com>
2025-02-01 01:01:43 +0900

    Load .env file in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:notion_task_app/ui/screens/task_list_screen.dart';
 
-void main() {
+void main() async {
+  await dotenv.load(fileName: '.env');
   runApp(const ProviderScope(child: MyApp()));
 }
 
