c0b69bed28597011304cea2807858370a411e5cc
Haruki1090 <haruharu.com.109607@gmail.com>
2025-01-31 23:45:19 +0900

    Wrap app in ProviderScope and add task list screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:notion_task_app/ui/screens/task_list_screen.dart';
 
 void main() {
   runApp(const ProviderScope(child: MyApp()));
